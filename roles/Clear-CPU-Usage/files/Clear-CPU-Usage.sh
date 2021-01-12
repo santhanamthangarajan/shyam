d54d70c9bc8fb14cba2935c93a8f8371c62359fd
egodasari <shyamkumar.d@orioninc.com>
2021-01-12 07:15:10 -0500

    code added

--- roles/Clear-CPU-Usage/files/Clear-CPU-Usage.sh
+++ roles/Clear-CPU-Usage/files/Clear-CPU-Usage.sh
@@ -9,7 +9,9 @@
 #top -n 1 -b  >> /tmp/cpu.txt
 #CurrentUsage=`cat cpu.txt | head -1 | awk '{print $12}' | sed 's/[,]//g'`
 Limit=6.10
-ps -eo pid,ppid,%mem,%cpu --sort=-%cpu | awk '{print $1,$4}' | sed '1d' >> /tmp/pidcpuusage.txt
+ps -eo pid,ppid,%mem,%cpu,cmd --sort=-%cpu | awk '{print $1,$4,$5}' | sed '1d' >> /tmp/pidcpuusage.txt
+sed -i '/python/d' /tmp/pidcpuusage.txt
+#ps -eo pid,ppid,%mem,%cpu --sort=-%cpu | awk '{print $1,$4}' | sed '1d' >> /tmp/pidcpuusage.txt
 PID_LIST=`cat /tmp/pidcpuusage.txt | awk '{print $1}'`
 for i in $PID_LIST
 do
